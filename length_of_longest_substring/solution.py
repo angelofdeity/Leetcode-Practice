e6c811c3417a4242b01c3b8130f68efe09d5e594
Nico <lawani.gk@gmail.com>
2023-06-24 09:53:17 +0100

    update solution

--- length_of_longest_substring/solution.py
+++ length_of_longest_substring/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pair = {}
-        w_start = 0
+        indexes = {}
+        offset = longest = 0
         for i in range(len(s)):
-            old_index = pair[s[i]]
-            if s[i] in pair and old_index >= w_start:
-                w_start = old_index
-                window = i - w_start
-            else:
-                pair[s[i]] = i
-         return window
-string = "abcabcdefgabc"
-expected = 7, output = 7
-string = "dkjvdfhju"
-expected = 6, output = 5
-print(Solution().lengthOfLongestSubstring(string))
+            char = s[i]
+            if char in indexes and indexes[char] >= offset:
+                window = i - offset
+                offset = indexes[char] + 1  # found a repeat
+                longest = max(window, longest)
+            indexes[char] = i
+        return max(longest, (len(s) - offset))
+
+
+s = "abbbcdde"
